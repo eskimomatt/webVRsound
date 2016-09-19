a0dace77a28768124b4392ed3caf8c7e0cf0a2a4
Matt Barringer <matt@eskimomatt.local>
2016-09-19 14:22:31 +0100

    adding initial files

---
+++ main.js
@@ -0,0 +1,31 @@
+
+jQuery(document).ready(function($) {
+  amount =10;
+  angle = 0;
+  step = (2*Math.PI) / amount;
+  radius=10;
+  angle=0;
+  for(var i=0;i<amount;i++){
+    var x = Math.round(radius * Math.cos(angle)),
+        y = Math.round(radius * Math.sin(angle));
+    $('a-scene').append('<a-entity position="'+x+' 0 '+y+'" cursor-listener><a-collada-model src="#bird" rotation="0 '+(angle*180)+' 0" scale="18 18 18"></a-collada-model><a-animation begin="mouseenter" attribute="rotation" to="0 360 0" dur="2000" fill="both" repeat="indefinite" easing="linear"></a-animation><a-animation begin="mouseleave" attribute="rotation" to="0 0 0" dur="2000" fill="both" easing="ease" repeat="none"></a-animation></a-entity>');
+    angle += step;
+  }
+
+
+
+  // Component to change to random color on click.
+  AFRAME.registerComponent('click-color-change', {
+    init: function () {
+      var COLORS = ['red', 'green', 'blue'];
+      this.el.addEventListener('click', function () {
+        var randomIndex = Math.floor(Math.random() * COLORS.length);
+        this.setAttribute('material', 'color', COLORS[randomIndex]);
+        console.log('I was clicked!');
+      });
+    }
+  });
+});
+
+
+
